f6e6677289c4209c79cf9238433c7524bb0abe42
Mark Canuel <mlcanuel@ucalgary.ca>
2024-11-23 13:57:01 -0700

    testing branch creation - created welcome/login folder and page

---
+++ travel-app/src/app/welcome-login/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "../components/Navbar";
+
+export default function WelcomeLoginPage() {
+    return (
+      <div>
+        <p>WelcomeLogin testing</p>
+        <Navbar/>
+      </div>
+    );
+  }
