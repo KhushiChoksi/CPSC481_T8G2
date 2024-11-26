f405f3ea0ceb6c05702b7e5babb616a8d6efe58e
muhammadadil02 <muhammadadil011002@gmail.com>
2024-11-25 17:25:41 -0700

    Initial setup commit

--- travel-app/src/app/explore/page.tsx
+++ travel-app/src/app/explore/page.tsx
@@ -1,10 +1,20 @@
 import Navbar from "../components/Navbar";
+import React from "react";
+
 
 export default function ExplorePage() {
   return (
-    <div>
-      <p>Explore testing</p>
-      <Navbar/>
-    </div>
-  );
+  <div style={{ 
+  textAlign: 'left', 
+  marginTop: '50px', 
+  marginLeft: '25px', 
+  fontSize: '35px',
+  color: 'black'}}>
+  <h1 style={{fontWeight: 'bold'}}>Explore Page </h1>
+
+  
+  <Navbar/>
+</div>
+);
+
 }
