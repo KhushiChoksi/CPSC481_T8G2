1cb93477d4a0db1f781b9ae3f13ef53695c9527d
Mark Canuel <mlcanuel@ucalgary.ca>
2024-12-02 13:09:55 -0700

    bug fixes for trip button display. ensured the display is fixed so there are no issues with display. the text length is 10 characters, if it is greater than 10 it will be truncated with "..". Additionally, trip limit is now 20 characters to ensure no issues with the displays.

--- travel-app/src/app/components/TripButton.tsx
+++ travel-app/src/app/components/TripButton.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import { FaPlaneDeparture } from "react-icons/fa";
-import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowDown, IoIosArrowBack } from "react-icons/io"; // Import both arrows
 import AddedTripsPopup from "./trip-settings/AddedTripsPopup";
 import { useTrip } from "../context/TripContext";
 
@@ -14,27 +14,73 @@ const TripButton: React.FC = () => {
     <>
       {/* Trip Button */}
       <div
-        className="flex items-center bg-black py-4 px-12 rounded-full cursor-pointer h-[56px]"
-        style={{ zIndex: 1001 }}
-        onClick={() => setShowPopup(!showPopup)}
+        className="relative bg-black py-4 px-4 rounded-full cursor-pointer h-[56px] overflow-hidden flex items-center justify-between"
+        style={{
+          zIndex: 1001,
+          width: "200px", // Fixed button width
+        }}
+        onClick={() => setShowPopup(!showPopup)} // Toggle popup visibility
       >
-        <FaPlaneDeparture
-          className={`text-[20px] -ml-6 mr-6 ${
-            showPopup ? "text-hl-orange" : "text-white"
-          }`}
-        />
-        <span
-          className={`text-[20px] mr-2 ${
-            showPopup ? "text-hl-orange" : "text-white"
-          }`}
+        {/* Flight Icon */}
+        <div
+          className="flex items-center justify-center"
+          style={{
+            width: "24px",
+            height: "24px",
+          }}
         >
-          {selectedTrip}
-        </span>
-        <IoIosArrowDown
-          className={`text-[14px] ml-6 -mr-7 ${
+          <FaPlaneDeparture
+            className={`transition-colors ${
+              showPopup ? "text-hl-orange" : "text-white"
+            }`}
+            style={{
+              width: "20px",
+              height: "20px",
+            }}
+          />
+        </div>
+
+        {/* Trip Name */}
+        <div
+          className={`flex-grow text-[20px] transition-colors ${
             showPopup ? "text-hl-orange" : "text-white"
           }`}
-        />
+          style={{
+            whiteSpace: "nowrap",
+            overflow: "hidden",
+            textOverflow: "clip", // Avoid CSS ellipsis
+            textAlign: "center", // Ensure text is centered between icons
+          }}
+        >
+          {selectedTrip?.length > 10 ? `${selectedTrip.slice(0, 8)}..` : selectedTrip}
+        </div>
+
+        {/* Arrow Icon */}
+        <div
+          className="flex items-center justify-center"
+          style={{
+            width: "16px",
+            height: "16px",
+          }}
+        >
+          {showPopup ? (
+            <IoIosArrowDown
+              className="transition-colors text-hl-orange"
+              style={{
+                width: "12px",
+                height: "12px",
+              }}
+            />
+          ) : (
+            <IoIosArrowBack
+              className="transition-colors text-white"
+              style={{
+                width: "12px",
+                height: "12px",
+              }}
+            />
+          )}
+        </div>
       </div>
 
       {/* Added Trips Popup */}
